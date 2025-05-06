5fe10dfbaa7e5607adcd76d4aa1a69bd48879ba3
YGN <ygntechstartup@gmail.com>
2025-05-06 12:19:07 +0530

    Added the CaseInsensitiveness

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,30 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
-    for line in search(&config.query, &content).unwrap_or_else(|err| {
-        println!("{err}");
-        vec!["ERROR"]
-    }) {
-        println!("{line}")
+    let result = if config.ignore_case {
+        search_case_insensitive(&config.query, &content)
+    } else {
+        search_case_sensitive(&config.query, &content)
+    };
+    match result {
+        Ok(lines) => {
+            for line in lines {
+                println!("{line}")
+            }
+        }
+        Err(err) => {
+            println!("{err}");
+            return Err(err).into();
+        }
     }
     Ok(())
 }
 
 pub struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -23,14 +34,39 @@ impl Config {
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
-pub fn search<'a>(query: &'a str, contents: &'a str) -> Result<Vec<&'a str>, Box<dyn Error>> {
+pub fn search_case_sensitive<'a>(
+    query: &'a str,
+    contents: &'a str,
+) -> Result<Vec<&'a str>, Box<dyn Error>> {
     let mut v = vec![];
     for line in contents.lines() {
         if line.contains(query) {
+            v.push(line.trim());
+        }
+    }
+    if v.is_empty() {
+        return Err("NOT FOUND".into());
+    }
+    Ok(v)
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &'a str,
+    contents: &'a str,
+) -> Result<Vec<&'a str>, Box<dyn Error>> {
+    let mut v = vec![];
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
             v.push(line.trim());
         }
     }
@@ -44,12 +80,27 @@ pub fn search<'a>(query: &'a str, contents: &'a str) -> Result<Vec<&'a str>, Box
 mod tests {
     use super::*;
     #[test]
-    fn one_result() {
-        let query = "Three";
+    fn case_sensitive() {
+        let query = "Pick";
         let contents = "\n
         Rust:\n
         safe,fast,productive.\n
         Pick Three.";
-        assert_eq!(vec!["Pick Three."], search(query, contents));
+        assert_eq!(
+            vec!["Pick Three."],
+            search_case_sensitive(query, contents).unwrap()
+        );
+    }
+    #[test]
+    fn case_insensitive() {
+        let query = "pick";
+        let contents = "\n
+        Rust:\n
+        safe,fast,productive.\n
+        Pick Three.";
+        assert_eq!(
+            vec!["Pick Three."],
+            search_case_insensitive(query, contents).unwrap()
+        );
     }
 }
