294eb9f58d23f361c2210233caf3756cd776ca0b
YGN <ygntechstartup@gmail.com>
2025-05-06 13:05:49 +0530

    Succesffully Done the Project

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    run(config).unwrap_or_else(|err| println!("Problem While Opening File:{err}"));
+    run(config).unwrap_or_else(|err| eprintln!("Problem While Opening File:{err}"));
 }
